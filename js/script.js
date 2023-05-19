728bcd860769f9af708ac5a68321ef9de20df6ee
lusino21 <lusianobonifacio6@gmail.com>
2023-05-19 19:00:26 -0300

    Arreglo JS

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 //Creaciacion de obajetos//
-function Productos(id, categoria, producto, imagen, precio, stock){
+function Productos(id, categoria, producto, imagen, precio, stock, info, marca){
         this.id = id;
         this.categoria = categoria; 
         this.producto = producto;
         this.imagen = imagen;
         this.precio = precio;
         this.stock = stock;
+        this.info = info;
+        this.marca = marca;
         this.restarstock = function() {
                 this.stock -= cantidad
         };
 }
-const productoF = new Productos (1, "Celular", "Funda celular", "../images/fundas-C.png", 350, 20)
-const productoA = new Productos (2, "Computadora", "Auriculares gamer", "../images/auricularG.png", 2500, 50)
-const productoM = new Productos (3, "Computadora","Mause gamer", "../images/mausseG.png", 2000, 45)
-const productoT = new Productos (4, "Computadora","Teclado gamer", "../images/tecladoG.png", 1500, 35)
-const productoSG = new Productos (5,"Computadora", "Silla gamer", "../images/sillaG.png", 50000, 25)
+const productoF = new Productos (1, "Celular", "Funda celular", "../images/fundas-C.png", 350, 20, "Protege tu celular de golpes y puedes encontrarlo en 2 diferentes estilos: blanco o negro, perfecto para modelos de Iphone 7/7 plus.","CeluWorld" )
+const productoA = new Productos (2, "Computadora", "Auriculares gamer", "../images/auricularG.png", 2500, 50, "Con los auriculares gamers VX70, llegó la hora de cambiar la experiencia con tus juegos, gracias a su sonido claro, potente y sus bajos inmersivos.","Tonet & Vender")
+const productoM = new Productos (3, "Computadora","Mause gamer", "../images/mausseG.png", 2000, 45, "Cualquier gamer experimentado conoce la importancia de un buen gaming mouse y aún más si su género favorito son los FPS (Shooters). Por eso te ofresemo MZ700.", "Pro ZC")
+const productoT = new Productos (4, "Computadora","Teclado gamer", "../images/tecladoG.png", 1500, 35, "Si queres sentirte como verdadero gamer no te puede faltar un buen teclado, El TCvol 3 te permite jugar como vos quieras ya que es mecanico.", "Ryzen")
+const productoSG = new Productos (5,"Computadora", "Silla gamer", "../images/sillaG.png", 50000, 25, "Si queres sentirte comodo la MY20 te va a dar la mejor sensacion de comodidad en el mercado, con asiento reclinable y apoya brazos.", "Myzerus")
 const productos = [productoF, productoA, productoM, productoT, productoSG]
 const disponibles = productos.filter((el) => el.stock > 0);
 const productosCatalogo = disponibles.map((el) => el.producto)
 //Variables//
 trolley = [];
+let ids = []
 let categorias = "";
 //DOM//
 let trolleyList = document.getElementById("trolley-list")
@@ -33,19 +36,28 @@ function cartas(cata){
         catalogo.innerHTML = ""
         for (const produc of cata){
                 let card = document.createElement("div");
-                card.className = "card";
+                card.className = "card carr";
                 let cardBody = document.createElement("div")
                 cardBody.className = "card-body"
                 cardBody.innerHTML =  `<img src="${produc.imagen}" class="card-img-top" alt="${produc.producto}">
-                                       <h5 class="card-title">${produc.producto}</h5>
+                                       <h5 class="card-title"><strong>${produc.producto}</strong></h5>
                                        <p class="card-text">${produc.precio}$</p>
                                        <p class="card-text">Stock: ${produc.stock}</p>`;
+                let blockBotton = document.createElement("div")
+                blockBotton.className = " d-flex justify-content-between"
                 let cardBotton = document.createElement("button");
+                let infocardBotton = document.createElement("button");
+                infocardBotton.className = "btn-b"
+                infocardBotton.innerText = "Ver"
+                infocardBotton.setAttribute("info", produc.id)
+                infocardBotton.addEventListener("click",infoCart)
                 cardBotton.className = "btn-c"
                 cardBotton.innerText = "Comprar"
                 cardBotton.setAttribute("mark", produc.id)
                 cardBotton.addEventListener("click",addCartToTrolley)
-                cardBody.append(cardBotton)
+                blockBotton.append(cardBotton)
+                blockBotton.append(infocardBotton)
+                cardBody.append(blockBotton)
                 card.append(cardBody)
                 catalogo.append(card);
         }
@@ -53,6 +65,29 @@ function cartas(cata){
 function addCartToTrolley(event){
         trolley.push(event.target.getAttribute("mark"))
         renderTrolley()
+        Toastify({
+                text: "Se agrago al Carrito",
+                className:"info",
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+              }).showToast();
+}
+function infoCart(event){
+        let id = event.target.getAttribute("info")
+        let item = productos.find ((producto) => {
+                return producto.id === parseInt(id)
+        })
+        console.log(item)
+        Swal.fire({
+                title: 'Informacion',
+                text:`${item.producto}: ${item.info}`,
+                footer: `<p>Marca: ${item.marca}</p>`,
+                imageUrl: `${item.imagen}`,
+                imageHeight: 300,
+              
+              })
+        
 }
 function renderTrolley(){
         //saveCartToStorage()
@@ -67,7 +102,7 @@ function renderTrolley(){
                         return id === itemId ? total += 1 : total
                 }, 0)
         let linea = document.createElement("li");
-        linea.className = "list-group-item"
+        linea.className = "list-group-item cartToCarr "
         linea.innerHTML = `<div class="d-flex justify-content-between align-items-start">
                            <div class="ms-2 me-auto">
                            <div class="fw-bold">${item[0].producto}</div>
